c67dab9aeb3063cea5fba2143df063020b8c567c
shinny-taojiachun <taojiachun@shinnytech.com>
2023-12-13 14:01:37 +0800

    venv路径去除version信息；dag统一放入 CDK_AIRFLOW_DAG_DIR 下，通过文件名称辨别所属venv

--- airflow/executors/local_executor.py
+++ airflow/executors/local_executor.py
@@ -93,8 +93,8 @@ class LocalWorkerBase(Process, LoggingMixin):
     def _execute_work_in_subprocess(self, command: CommandType) -> str:
         try:
             # shinny-airflow 需要以dag组whl文件为最小单元创建单独的venv环境
-            # venv 存放在CDK_AIRFLOW_VENV_BASE_DIR下，以venv_name/venv_version命名
-            # 如 ${CDK_AIRFLOW_VENV_BASE_DIR}/mdreal/1.0.0/update_mdreal_cont_info.py
+            # venv 存放在CDK_AIRFLOW_VENV_BASE_DIR下，以service_name命名
+            # dag放置在CDK_AIRFLOW_DAG_DIR下，如 ${CDK_AIRFLOW_DAG_DIR下}/mdreal-update_mdreal_cont_info.py
             # 需要使用非sqlite数据库并设置 [core]/execute_tasks_new_python_interpreter = True
             # 首先，在运行时需要设置不同的PATH环境变量以指向各自venv/bin下的airflow wrapper
             # command以airflow开头，因此更换PATH可以指向不同的venv
@@ -103,8 +103,9 @@ class LocalWorkerBase(Process, LoggingMixin):
             # import 命令: airflow dag-processor -S /path/to/specific/dags -n 1
             if "--subdir" in command and "CDK_AIRFLOW_VENV_BASE_DIR" in os.environ:
                 dag_file_name = pathlib.Path(command[command.index("--subdir") + 1])
-                venv_name, venv_version = dag_file_name.parent.parent.name, dag_file_name.parent.name
-                venv_dir = pathlib.Path(os.getenv("CDK_AIRFLOW_VENV_BASE_DIR")) / venv_name / venv_version
+                # otg-sim-update_mdreal_cont_info.py -> otg-sim
+                venv_name = dag_file_name.name.rsplit("-", 1)[0]
+                venv_dir = pathlib.Path(os.getenv("CDK_AIRFLOW_VENV_BASE_DIR")) / venv_name
                 if venv_dir.is_dir():
                     self.log.info(f"{dag_file_name} will be execute using interpreter of venv: {venv_dir}")
                     # 准备子进程的运行环境，将venv排在第一个并将系统bin目录包括在内
